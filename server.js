c5b937e7c2e2023ae649f1703f01a11a8831338a
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-06 17:02:27 -0300

    add conexion whit db archives and .env too

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
-const routes = require('./routes')
 const app = express();
+
+const mongoose = require('mongoose')
+
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(process.env.CONEXIONSTRING,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=> {
+    app.emit('pronto')
+})
+.catch(e => console.log(e))
+
+
+const routes = require('./routes')
 const path = require('path')
 
-
-
 app.use(express.urlencoded({extended:true}) )  // exibir os dados das requisições POST
+
 app.use(express.static('./public'))  // exibir os conteudos estáticos
 
-app.use(routes);
+app.use(routes); //Nosso proprio midleware
+
 app.set('views', path.resolve(__dirname,'src','views'))
 app.set('view engine', 'ejs')
 

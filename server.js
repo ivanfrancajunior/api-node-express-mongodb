79b1dc973b360dcce6b504abc1d55a489d0ad1a0
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-06 10:15:42 -0300

    class leason: req. params and query str.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(
+    express.urlencoded({extended:true})
+)
+
 app.get('/', (req, res) => {
     res.send(`
     <form action='/' method='POST'>
@@ -8,9 +12,18 @@ app.get('/', (req, res) => {
     <button>Enviar</button>
     </form>`)
 });
+app.get('/teste', (req, res) => {
+    
+    //params teste/profile/id||profile etc.
+    //query  teste/profile/?chave=valor etc
+    
+    console.log(req.params)
+    console.log(req.quety)
+    res.send(``)
+});
 
 app.post('/', (req, res) => {
-    res.send(' Formulário enviado!')
+    res.send(`O q vc enviou foi ${req.body.name}`)
 });
 
 app.listen(3000, () => console.log ('🔥 server up! on port 3000'))

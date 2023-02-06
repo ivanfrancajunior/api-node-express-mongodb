253c109f598dd26ba859bfba7c6e0fc83a24427e
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-06 11:50:43 -0300

    add views folder, views archive and set configs.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const routes = require('./routes')
 const app = express();
+const path = require('path')
 
-/* app.use(
-    express.urlencoded({extended:true}) // exibir os dados das requisições POST
-) */
+app.use(express.urlencoded({extended:true}) )  // exibir os dados das requisições POST
 
 app.use(routes);
+app.set('views', path.resolve(__dirname,'src','views'))
+app.set('view engine', 'ejs')
 
 
 
